53b6f6fc49d5e89a07ef41d88353c2c48ea9ed12
Jaclav <kancelariajkw@gmail.com>
2020-11-02 14:04:40 +0100

    Fixed linux makefile

--- Linux.mk
+++ Linux.mk
@@ -1,8 +1,8 @@
 COMPILER=g++
 CFLAGS=-pedantic -Wextra -Wall -fexceptions -std=c++2a -Iinclude -s -Os -c
 
-run: bin obj obj/main.o obj/src/Button.o obj/src/Console.o obj/src/Game.o obj/src/Ini.o obj/src/Items.o obj/src/Menu.o obj/src/Player.o obj/src/tools.o obj/src/World.o
-	$(COMPILER) -o bin/kopalnerzemioslo obj/main.o obj/src/Button.o obj/src/Console.o obj/src/Game.o obj/src/Ini.o obj/src/Items.o obj/src/Menu.o obj/src/Player.o obj/src/tools.o  obj/src/World.o -s -lsfml-window -lsfml-system -lsfml-graphics -lsfml-audio
+run: bin obj obj/main.o obj/src/Button.o obj/src/Console.o obj/src/Game.o obj/src/Ini.o obj/src/Inventory.o obj/src/Items.o obj/src/Menu.o obj/src/Player.o obj/src/tools.o obj/src/World.o
+	$(COMPILER) -o bin/kopalnerzemioslo obj/main.o obj/src/Button.o obj/src/Console.o obj/src/Game.o obj/src/Ini.o obj/src/Inventory.o obj/src/Items.o obj/src/Menu.o obj/src/Player.o obj/src/tools.o  obj/src/World.o -s -lsfml-window -lsfml-system -lsfml-graphics -lsfml-audio
 
 bin:
 	mkdir bin
@@ -25,6 +25,9 @@ obj/src/Game.o: src/Game.cpp
 
 obj/src/Ini.o: src/Ini.cpp
 	$(COMPILER) $(CFLAGS) src/Ini.cpp -o obj/src/Ini.o
+	
+obj/src/Inventory.o: src/Inventory.cpp
+	$(COMPILER) $(CFLAGS) src/Inventory.cpp -o obj/src/Inventory.o
 
 obj/src/Items.o: src/Items.cpp
 	$(COMPILER) $(CFLAGS) src/Items.cpp -o obj/src/Items.o
